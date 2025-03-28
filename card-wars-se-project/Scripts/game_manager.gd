e185bcd7bc2922a2fb46dc4df2d40af6c51a2fef
bsearight <benjamin.searight@western.edu>
2025-03-28 15:55:51 -0600

    Updated Enemy Logic
    Bug: repeat count will increment abnormally when switching back to attack after healing

--- card-wars-se-project/Scripts/game_manager.gd
+++ card-wars-se-project/Scripts/game_manager.gd
@@ -6,13 +6,18 @@ extends Node
 
 const PLAYER_MAX_HEALTH = 100 # subject to change in the future
 const ENEMY_MAX_HEALTH = 100 # subject to change in the the future
+const ENEMY_ACTIONS = [ENEMY_ACTION_ATTACK, ENEMY_ACTION_ATTACK, ENEMY_ACTION_ATTACK, ENEMY_ACTION_HEAL, ENEMY_ACTION_HEAL]
+const ENEMY_ACTION_ATTACK = 0
+const ENEMY_ACTION_HEAL = 1
+var enemy_action_repeat = 0
 var enemy_health: int = 100
 var player_health: int = 100
 var player_turn: bool = true
 var player_def: int = 0
 var card_types = ["attack", "heal", "defend"] 
 var combat_instance = null  # Declare combat_instance globally
-var enemy_next_action = 0
+var enemy_next_action = ENEMY_ACTION_ATTACK
+var enemy_action = ENEMY_ACTION_ATTACK
 
 
 func _ready():
@@ -45,6 +50,10 @@ func start_combat():
 
 
 func player_action(action: String):
+	if player_health <= 0:
+			print("Enemy wins. Switching to game over.")
+			get_tree().change_scene_to_file("res://Scenes/combat_defeat.tscn")
+			return
 	var combat_scene = get_tree().root.get_node_or_null("CombatScene")
 	print("Player action triggered:", action)
 	
@@ -90,6 +99,10 @@ func player_action_defend(value: int):
 
 
 func enemy_turn():
+	if enemy_health <= 0:
+			print("player wins. switch to victory scene")
+			get_tree().change_scene_to_file("res://Scenes/combat_victory.tscn")
+			return
 	var combat_scene = get_tree().root.get_node_or_null("CombatScene")
 	print("Enemy's turn started...")
 	await get_tree().create_timer(0.75).timeout 
@@ -97,15 +110,16 @@ func enemy_turn():
 	if (not combat_scene):
 		print("ERROR: CombatScene not found!")
 		return
-	#var enemy_action = randi() % 2 #old attack action 
 	var enemy_action = enemy_next_action
-	enemy_next_action = randi() % 2
+	enemy_next_action = enemy_action_choose()
+
 	combat_scene.updateEnemyNextMoveIndicator(enemy_next_action)
 	
-	print("Enemy chose action:", enemy_action)
+	print("Enemy current action:", enemy_action)
+	print("Enemy next action:", enemy_next_action)
 
 	# attack for 15, heal for 10
-	enemy_action_exectute(enemy_action, 15 if enemy_action == 0 else 10, combat_scene)
+	enemy_action_execute(enemy_action, 15 if enemy_action == ENEMY_ACTION_ATTACK else 10, combat_scene)
 
 	player_def = 0
 
@@ -117,13 +131,39 @@ func enemy_turn():
 
 	end_turn(combat_scene)
 
-func enemy_action_exectute(action: int, value: int, combat_scene: Node):
-	if action == 0:
+func enemy_action_choose() -> int:
+	var next = ENEMY_ACTIONS.pick_random() 
+	if enemy_health == ENEMY_MAX_HEALTH:
+			enemy_action_repeat += 1
+			print("Enemy action repeat count:", enemy_action_repeat)
+			return ENEMY_ACTION_ATTACK
+	else:
+		if enemy_action == next:
+			print("Enemy action is the same as last turn, incrementing repeat count")
+			enemy_action_repeat += 1
+		else:
+			enemy_action_repeat = 0
+			print("Enemy action changed, reset repeat count")
+		if enemy_action_repeat == 1: # if the enemy tries to heal twice in a row, it will attack instead
+			if next == ENEMY_ACTION_HEAL:
+				enemy_action_repeat = 0
+				print("Forced enemy action change to attack")
+				return ENEMY_ACTION_ATTACK
+		elif enemy_action_repeat > 3: # if the enemy tries to attack four times (or more) in a row, it will heal instead
+			if next == ENEMY_ACTION_ATTACK:
+				enemy_action_repeat = 0
+				print("Forced enemy action change to heal")
+				return ENEMY_ACTION_HEAL
+	print("Enemy action repeat count:", enemy_action_repeat)
+	return next
+
+func enemy_action_execute(action: int, value: int, combat_scene: Node):
+	if action == ENEMY_ACTION_ATTACK:
 		var damage = max(0, value - player_def) 
 		player_health -= damage
 		combat_scene.PlayerTakeDamage()
 		print("Enemy attacks! Player Health:", player_health)
-	elif action == 1:
+	elif action == ENEMY_ACTION_HEAL:
 		combat_scene.playEnemyHealAnimation() 
 		if enemy_health < ENEMY_MAX_HEALTH:
 			enemy_health += value
@@ -137,14 +177,6 @@ func enemy_action_exectute(action: int, value: int, combat_scene: Node):
 
 # Ends the turn and switches between player and enemy 
 func end_turn(combat_scene: Node):
-	if enemy_health <= 0:
-			print("player wins. switch to victory scene")
-			get_tree().change_scene_to_file("res://Scenes/combat_victory.tscn")
-			return
-	if player_health <= 0:
-			print("Enemy wins. Switching to game over.")
-			get_tree().change_scene_to_file("res://Scenes/combat_defeat.tscn")
-			return
 	print("Ending turn. Player's turn before toggle:", player_turn)
 	player_turn = !player_turn  # Toggle turn
 	if combat_scene:
